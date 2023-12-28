787c8e77e598fb3caef67e52f6ae996641f8b721
EmanTurk <143930452+EmanTurk@users.noreply.github.com>
2023-12-28 02:33:44 +0200

    new branch

---
+++ src/classExercise/try1.js
@@ -0,0 +1,14 @@
+// import express from 'express';
+
+// const server = express();
+
+// server.get ('/msg', (req,res)=>{
+//     res.send ("hi from s")
+// });
+// server.put ('/msg', (req,res)=>{});
+// server.get ('/about', (req,res)=>{});
+
+// server.listen(4747, ()=>{
+//     console.log ('express listesn')
+// })
+
